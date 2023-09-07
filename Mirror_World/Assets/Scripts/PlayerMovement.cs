3b1ed61d31722db73e31c09e7f37a5819897a081
Akshat20105 <akshatraj20105@gmail.com>
2023-09-07 18:41:54 +0530

    draft3

--- Mirror_World/Assets/Scripts/PlayerMovement.cs
+++ Mirror_World/Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D a;
     private BoxCollider2D coll;
-    public bool isJumping=false;
+    // public bool iswalking=false;
+    private Animator anime;
     public LayerMask jumpableGround;
+    private SpriteRenderer sp;
     // Start is called before the first frame update
     void Start()
     {
         a=GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        anime = GetComponent<Animator>();
+        sp=GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float x =Input.GetAxis("Horizontal");
-        a.velocity=new Vector2(x * 7f,a.velocity.y);
+        a.velocity=new Vector2(x * 5f,a.velocity.y);
         if(Input.GetKeyDown("space") && IsGrounded()){
             a.velocity=new Vector2(a.velocity.x,10);
-            isJumping=true;
         }
+        if(x!=0f) anime.SetBool("iswalking",true);
+        else anime.SetBool("iswalking",false);
+        if(x>0f) sp.flipX=true;
+        else if(x<0f) sp.flipX=false;
      }
     private bool IsGrounded(){
         return Physics2D.BoxCast(coll.bounds.center,coll.bounds.size,0f, Vector2.down,.1f,jumpableGround);
